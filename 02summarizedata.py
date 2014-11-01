b08f27a321f89a4629a88e355c9b984a5594fe29
vonpupp <vonpupp@gmail.com>
2014-11-01 03:54:45 +0000

    Added CI figures

--- 02summarizedata.py
+++ 02summarizedata.py
@@ -97,6 +97,7 @@ if __name__ == "__main__":
         p.set_data(per_algorithm_summary)
         #p.plot_all_algorithm_comparison(host, trace)
         p.plot_all_algorithm_comparison(host)
+        p.plot_all_confidence_interval_comparison(host)
 
         for vms in range(vms_start, vms_stop, vms_step):
             for algorithm in algorithm_scenarios:
