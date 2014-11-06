d696b55451968f52febc2ce043c6d5abedd0ab52
vonpupp <vonpupp@gmail.com>
2014-11-06 00:14:02 +0000

    Added Google vms generation tool and config files for experiment3

---
+++ datacenter/experiment3/generate-vms.sh
@@ -0,0 +1,1 @@
+python ../../tools-datacenter/google/gen-vms.py -vm 200 -i ../../google-workload-traces/filtered-cpu-29-40.csv -o vms-google-mean-29-40.json
---
+++ tools-datacenter/google/01sync.sh
@@ -0,0 +1,1 @@
+rsync -av --progress thiagomezuro:/mnt/disk1/mac5910/google-traces/tools/traces-albert/ ../../google-workload-traces/
