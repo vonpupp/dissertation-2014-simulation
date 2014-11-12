4268d7017e64e276a3a8940f003b8d23b9283bd1
vonpupp <vonpupp@gmail.com>
2014-11-12 01:49:46 +0000

    Added configs

---
+++ datacenter/experiment6/generate-servers.sh
@@ -0,0 +1,2 @@
+python ../../tools-datacenter/gen-servers.py -pm 200 -spec power_ssj2008-20140615-00658.html -o servers-200-20140615-00658.json
+python ../../tools-datacenter/gen-servers.py -pm 200 -spec power_ssj2008-20140804-00662.html -o servers-200-20140804-00662.json
---
+++ datacenter/experiment6/generate-vms.sh
@@ -0,0 +1,1 @@
+python ../../tools-datacenter/google/gen-vms.py -vm 200 -i filtered-data.csv -o vms-google-mean-15-20-stdev.json
