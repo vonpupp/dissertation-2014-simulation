f59837c6ab08b9f07cb98e1f087e20a5d3460a36
vonpupp <vonpupp@gmail.com>
2014-11-19 00:28:48 +0000

    Added execute-01simulate.sh

---
+++ env-experiments/execute-01simulate.sh
@@ -0,0 +1,8 @@
+cd ..
+python 01simulate.py $LINEARCONF
+python 01simulate.py $NONLINEAR1CONF
+python 01simulate.py $NONLINEAR2CONF
+python 01simulate.py $NONLINEAR3CONF
+python 01simulate.py $NONLINEAR4CONF
+python 01simulate.py $NONLINEAR5CONF
+python 01simulate.py $NONLINEAR6CONF
